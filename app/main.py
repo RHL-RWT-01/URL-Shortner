4bebd079a7b6c2be92e9afd9f705fd0cc3cdbdf6
Rahul Rawat <rahulrawat4060@gmail.com>
2025-07-24 20:35:02 +0000

    Integrated my RWLock

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ from flask import Flask, jsonify, request, redirect
 from app.utils import generate_id, is_url_valid
 from app.models import save_url, increment_click_count, get_stats
 from app.store import url_db
-import threading
-
-lock = threading.Lock()
+from app.threading import RWLock 
+lock = RWLock()
 
 app = Flask(__name__)
 BASE_URL = "http://127.0.0.1:5000/"
@@ -34,7 +33,8 @@ def shorten_url():
     if not original_url or not is_url_valid(original_url):
         return jsonify({"error": "Invalid URL"}), 400
 
-    with lock:
+    lock.acquire_write()
+    try:
         for sid, val in url_db.items():
             if val["long_url"] == original_url:
                 return jsonify({
@@ -48,6 +48,8 @@ def shorten_url():
             short_id = generate_id()
 
         save_url(short_id, original_url)
+    finally:
+        lock.release_write()
 
     return jsonify({
         "short_url": BASE_URL + short_id,
@@ -58,22 +60,33 @@ def shorten_url():
 
 @app.route("/<short_id>")
 def redirect_to_url(short_id):
-    with lock:
+    lock.acquire_read()
+    try:
         if short_id not in url_db:
             return jsonify({"error": "URL not found"}), 404
         url = url_db[short_id]["long_url"]
+    finally:
+        lock.release_read()
+
+    lock.acquire_write()
+    try:
         increment_click_count(short_id)
+    finally:
+        lock.release_write()
+
     return redirect(url)
 
 
 @app.route("/api/stats/<short_id>")
 def url_stats(short_id):
-    with lock:
-        try:
-            stats = get_stats(short_id)
-            return jsonify(stats)
-        except ValueError as e:
-            return jsonify({"error": str(e)}), 404
+    lock.acquire_read()
+    try:
+        stats = get_stats(short_id)
+        return jsonify(stats)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 404
+    finally:
+        lock.release_read()
 
 
 if __name__ == "__main__":
